16900365c892382aa5168a96cfd94393770eab01
Geoffrey Gilmore <ggilmore@geoffresonalmbp.lan>
2020-07-15 23:45:28 -0700

    add exit code back to prettier-check

--- scripts/ci/check-prettier.sh
+++ scripts/ci/check-prettier.sh
@@ -6,4 +6,5 @@ set -euxo pipefail
 yarn
 if ! yarn run prettier-check; then
   echo "The files listed above aren't correctly formatted. Please run 'yarn prettier' and commit the result."
+  exit 1
 fi
